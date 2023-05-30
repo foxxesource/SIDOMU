42edec3701d996778f614f6c72f8028d7c585b81
foxxesource <bintang.duinata31@gmail.com>
2023-05-30 20:50:19 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect, url_for, request, jsonify
+from pymongo import MongoClient
+import jwt
+from dotenv import load_dotenv
+import os
+from os.path import join, dirname
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+dotenv_path = join(dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+
+MONGODB_URL = os.environ.get("MONGODB_URL")
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URL)
+db = client[DB_NAME]
+
+SECRET_KEY = "finalproject"
+
+TOKEN_KEY= "mytoken"
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000, debug=True)
