61697e60d0bbdd571b1796c2ec06e3323083d489
foxxesource <bintang.duinata31@gmail.com>
2023-06-01 00:07:12 +0700

    commit login register modified

--- app.py
+++ app.py
@@ -33,5 +33,13 @@ def hospitals():
 def hospital1():
     return render_template("hospitals-item.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000, debug=True)
