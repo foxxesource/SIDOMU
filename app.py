92be6a04579ce2f7248d36db4518d26c15327b90
Sriraihanputr <sri.200170009@mhs.unimal.ac.id>
2023-05-31 22:02:18 +0700

    update

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
-from flask import Flask, render_template, redirect, url_for, request, jsonify
+from bson.objectid import ObjectId
+from flask import Flask, render_template, request, redirect
 from pymongo import MongoClient
-import jwt
-from dotenv import load_dotenv
-import os
-from os.path import join, dirname
-from werkzeug.utils import secure_filename
+import certifi
+ca = certifi.where()
+
 
 app = Flask(__name__)
-
-dotenv_path = join(dirname(__file__), ".env")
-load_dotenv(dotenv_path)
-
-MONGODB_URL = os.environ.get("MONGODB_URL")
-DB_NAME = os.environ.get("DB_NAME")
-
-client = MongoClient(MONGODB_URL)
-db = client[DB_NAME]
+client = MongoClient('mongodb+srv://sriraihanputri:070502@cluster0.k5lmeah.mongodb.net/?retryWrites=true&w=majority', tlsCAFile = ca)
+db = client.projectakhir
 
 SECRET_KEY = "finalproject"
 
@@ -25,13 +17,17 @@ TOKEN_KEY= "mytoken"
 def home():
     return render_template("index.html")
 
-@app.route("/hospitals", methods=["GET"])
-def hospitals():
-    return render_template("hospital.html")
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
-@app.route("/hospital1", methods=["GET"])
-def hospital1():
-    return render_template("hospitals-item.html")
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000, debug=True)
