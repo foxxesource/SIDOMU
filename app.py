a65ec7df0b1ab923cf8932cb5bcc916a0d35e00c
foxxesource <bintang.duinata31@gmail.com>
2023-06-01 16:24:14 +0700

    added about page and more pages

--- app.py
+++ app.py
@@ -45,5 +45,9 @@ def login():
 def login_doctor():
     return render_template("login-doctor.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000, debug=True)
